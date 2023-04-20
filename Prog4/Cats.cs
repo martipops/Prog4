6749c842ed358779ab1570a2686952b506114e74
martipops <melonnemann@gmail.com>
2023-04-19 20:55:43 -0400

    update

---
+++ Prog4/Cats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Prog4;
+
+public class Cats
+{
+    private bool isAdopted;
+
+    public Cats(string name, string breed, string sex, int age, string furColor, double price)
+    {
+        CatName = name;
+        CatBreed = breed;
+        CatGender = sex;
+        CatAge = age;
+        CatFurColor = furColor;
+        CatPrice = price;
+        isAdopted = false;
+    }
+
+    public string CatName { get; set; }
+    public string CatBreed { get; set; }
+    public string CatGender { get; set; }
+
+    private int catAge;
+    public int CatAge
+    {
+        get { return catAge; }
+        set
+        {
+            catAge = (value > 0) ? value : 3;
+        }
+    }
+
+    public string CatFurColor { get; set; }
+    public double CatPrice { get; set; }
+
+    public void IsAvailable()
+    {
+        isAdopted = false;
+    }
+
+    public void IsAdopted()
+    {
+        isAdopted = true;
+    }
+
+    public bool Availability()
+    {
+        return isAdopted;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {CatName}{Environment.NewLine}Breed: {CatBreed}{Environment.NewLine}Gender: {CatGender}{Environment.NewLine}Age: {CatAge}{Environment.NewLine}Fur Color: {CatFurColor}{Environment.NewLine}Price: {CatPrice}{Environment.NewLine}Adopted: {isAdopted}";
+    }
+}
---
+++ Prog4/Program.cs
@@ -0,0 +1,68 @@
+﻿using Prog4;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cats[] cats = new Cats[]
+        {
+            new Cats("Mittens", "Persian", "Female", 4, "White", 500),
+            new Cats("Whiskers", "Siamese", "Male", 2, "Brown", 450),
+            new Cats("Simba", "Maine Coon", "Male", 6, "Orange", 600),
+            new Cats("Luna", "Sphynx", "Female", 3, "Grey", 1000),
+            new Cats("Oliver", "Ragdoll", "Male", 1, "White", 700)
+        };
+
+        Dogs[] dogs = new Dogs[]
+        {
+            new Dogs("Buddy", "Labrador Retriever", "Male", 3, "Yellow", 800),
+            new Dogs("Molly", "Golden Retriever", "Female", 5, "Golden", 900),
+            new Dogs("Max", "German Shepherd", "Male", 4, "Black", 950),
+            new Dogs("Bella", "Poodle", "Female", 6, "White", 850),
+            new Dogs("Charlie", "Bulldog", "Male", 2, "Brindle", 1200)
+        };
+
+        PrintCats(cats);
+        Console.WriteLine();
+        PrintDogs(dogs);
+
+        cats[0].CatName = "Fluffy";
+        cats[0].CatAge = 5;
+        dogs[0].DogPrice = 750;
+        dogs[1].IsAdopted();
+
+        Console.WriteLine("\nUpdated Data:");
+        PrintCats(cats);
+        Console.WriteLine();
+        PrintDogs(dogs);
+
+        foreach (Dogs dog in dogs)
+        {
+            dog.IsAdopted();
+        }
+
+        Console.WriteLine("\nAll Dogs Adopted:");
+        PrintCats(cats);
+        Console.WriteLine();
+        PrintDogs(dogs);
+    }
+
+    static void PrintCats(Cats[] cats)
+    {
+        foreach (Cats cat in cats)
+        {
+            Console.WriteLine(cat.ToString());
+            Console.WriteLine();
+        }
+    }
+
+    static void PrintDogs(Dogs[] dogs)
+    {
+        foreach (Dogs dog in dogs)
+        {
+            Console.WriteLine(dog.ToString());
+            Console.WriteLine();
+        }
+    }
+}
